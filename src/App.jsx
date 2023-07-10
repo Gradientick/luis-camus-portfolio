f877261e7b3f753034b0b3013175999f4c8c4ace
gradientick <camus.luis95@gmail.com>
2023-07-10 23:07:36 +0800

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import NavBar from "./components/NavBar";
 function App() {
-  return <>Luis Camus pogi</>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <div>
+      <h1 className="bg-slate-900">hello</h1>
+      <h2>World</h2>
+    </div>
+  );
+}
+
+export default NavBar;
